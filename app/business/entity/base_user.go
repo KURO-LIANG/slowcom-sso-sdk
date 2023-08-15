c2db6788dadccb491d65d2ad72de6d0594bb450c
梁庆 <245374011@qq.com>
2023-08-15 16:51:15 +0800

    增加渠道id

--- app/business/entity/base_user.go
+++ app/business/entity/base_user.go
@@ -1,10 +1,11 @@
 package entity
 
 type BaseUserInfo struct {
-	Id       uint64 `json:"id" description:"id"`                // id
-	NickName string `json:"nickName" description:"用户昵称"`        // 用户昵称
-	Avatar   string `json:"avatar" description:"用户头像"`          // 用户头像
-	Phone    string `json:"phone" description:"手机号"`            // 手机号
-	MaOpenId string `json:"maOpenId" description:"小程序openid"` // 小程序openid
-	UnionId  string `json:"unionId" description:"微信开放平台id"`     // 微信开放平台id
+	Id        uint64 `json:"id" description:"id"`         // id
+	NickName  string `json:"nickName" description:"用户昵称"` // 用户昵称
+	Avatar    string `json:"avatar" description:"用户头像"`   // 用户头像
+	Phone     string `json:"phone" description:"手机号"`     // 手机号
+	ChannelId uint64 `json:"channelId" description:"渠道ID"`
+	MaOpenId  string `json:"maOpenId" description:"小程序openid"` // 小程序openid
+	UnionId   string `json:"unionId" description:"微信开放平台id"`   // 微信开放平台id
 }
