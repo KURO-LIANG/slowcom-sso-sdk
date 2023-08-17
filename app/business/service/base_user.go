e9dbe75101a0cc993719e269558a653e30974969
梁庆 <245374011@qq.com>
2023-08-17 14:05:33 +0800

    增加用户查询不同方法

--- app/business/service/base_user.go
+++ app/business/service/base_user.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"fmt"
 	"github.com/kuro-liang/slowcom-sso-sdk/app/business/entity"
 	"github.com/kuro-liang/slowcom-sso-sdk/http"
 )
@@ -10,8 +11,26 @@ type SSORequest struct {
 }
 
 // GetUserInfo 获取用户登录的信息
-func (s *SSORequest) GetUserInfo(phone string) (resData *entity.BaseUserInfo, err error) {
+func (s *SSORequest) GetUserInfo() (resData *entity.BaseUserInfo, err error) {
+	res, err := s.SSOClient.Get("/api/wx/user/info")
+	if err == nil {
+		resData = res.Data.(*entity.BaseUserInfo)
+	}
+	return
+}
+
+// GetUserInfoByPhone 通过手机号查询用户信息
+func (s *SSORequest) GetUserInfoByPhone(phone string) (resData *entity.BaseUserInfo, err error) {
 	res, err := s.SSOClient.Get("/api/wx/user/info?phone=" + phone)
+	if err == nil {
+		resData = res.Data.(*entity.BaseUserInfo)
+	}
+	return
+}
+
+// GetUserInfoById 通过用户id查询用户信息
+func (s *SSORequest) GetUserInfoById(id uint64) (resData *entity.BaseUserInfo, err error) {
+	res, err := s.SSOClient.Get(fmt.Sprint("/api/wx/user/info?id=", id))
 	if err == nil {
 		resData = res.Data.(*entity.BaseUserInfo)
 	}
