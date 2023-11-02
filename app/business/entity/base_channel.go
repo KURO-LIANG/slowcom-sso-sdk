82e6f2d9f2688345969abec73c5b2772d4d60570
梁庆 <245374011@qq.com>
2023-11-02 15:47:49 +0800

    修改渠道请求接口

--- app/business/entity/base_channel.go
+++ app/business/entity/base_channel.go
@@ -1,1 +1,11 @@
 package entity
+
+type ChannelInfo struct {
+	Id            uint64 `json:"id" description:"渠道ID"`                    // 渠道ID
+	ChannelName   string `json:"channelName" description:"渠道名称"`           // 渠道名称
+	MiniAppId     string `json:"miniAppId" description:"小程序appid"`         // 小程序appid
+	MiniAppSecret string `json:"miniAppSecret" description:"小程序appSecret"` // 小程序appSecret
+	AppId         string `json:"appId" description:"公众号appid"`             // 公众号appid
+	AppSecret     string `json:"appSecret" description:"公众号appSecret"`     // 公众号appSecret
+	TemplateId    string `json:"templateId" description:"公众号模板消息ID"`       // 公众号模板消息ID
+}
--- app/business/service/platform_channel.go
+++ app/business/service/platform_channel.go
@@ -1,8 +1,10 @@
 package service
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/kuro-liang/slowcom-sso-sdk/app/business/entity"
 	"github.com/kuro-liang/slowcom-sso-sdk/http"
 )
 
@@ -24,14 +26,17 @@ type SSOChannelRequest struct {
 //}
 
 // GetChannelById 获取平台信息
-func (s *SSOChannelRequest) GetChannelById(id uint64) (list interface{}, err error) {
+func (s *SSOChannelRequest) GetChannelById(id uint64) (resData entity.ChannelInfo, err error) {
 	res, err := s.SSOChannelClient.Get(fmt.Sprint("/platform/v1/channel/info?id=", id))
 	if err == nil {
 		if res.Code != 0 {
 			err = errors.New(res.Message)
 			return
 		}
-		list = res.Data
+		var mapData = res.Data.(map[string]interface{})
+		channel := mapData["channel"].(map[string]interface{})
+		d, _ := json.Marshal(channel)
+		_ = json.Unmarshal(d, &resData)
 	}
 	return
 }
