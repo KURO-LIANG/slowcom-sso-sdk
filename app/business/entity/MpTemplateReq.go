5a21315a4a8c4118ddff76dd3797ad4f525c1e0e
梁庆 <245374011@qq.com>
2023-12-13 13:36:13 +0800

    增加公众号模板通知

--- app/business/entity/MpTemplateReq.go
+++ app/business/entity/MpTemplateReq.go
@@ -4,6 +4,7 @@ type MpTemplateReq struct {
 	TemplateDataMap map[string]TemplateDataItem `json:"templateDataMap" v:"required#消息内容不能为空" description:"消息内容"`
 	UserId          uint64                      `json:"userId" v:"required#通知用户Id不能为空" description:"通知用户ID"`
 	PagePath        string                      `json:"pagePath" description:"小程序页面地址"`
+	MiniAppId       string                      `json:"miniAppId" description:"小程序APPID"`
 }
 type TemplateDataItem struct {
 	Value string `json:"value"`
