4bb22f399834e405fa82711c1bae53ea4cae30be
梁庆 <245374011@qq.com>
2024-04-25 17:09:15 +0800

    请求错误处理

--- http/response.go
+++ http/response.go
@@ -8,17 +8,19 @@ import (
 )
 
 type BaseRes struct {
-	Code    int    `json:"code" description:"响应码 0-成功"`
-	Message string `json:"message" description:"响应消息"`
-	Data interface{} `json:"data" description:"数据"`
+	Code    int         `json:"code" description:"响应码 0-成功"`
+	Message string      `json:"message" description:"响应消息"`
+	Data    interface{} `json:"data" description:"数据"`
 }
 
 // checkResponse 校验请求
 func checkResponse(res *httpclient.Response, requestError error) (baseRes *BaseRes, err error) {
 	if requestError != nil {
+		fmt.Printf("请求服务异常：%s", requestError.Error())
 		return nil, serror.New(405, "请求服务异常：请求超时")
 	}
 	if res.Response.StatusCode != 200 {
+		fmt.Printf("请求服务异常：%d", res.Response.StatusCode)
 		return nil, serror.New(res.Response.StatusCode, fmt.Sprint(res.Response.StatusCode, " 请求服务异常"))
 	}
 	bytes, err := res.ReadAll()
